841cf953c091d58569f939ec293227be9253b0b9
Basling <basling99@gmail.com>
2025-07-24 15:41:11 +0530

    Created a chatbook project

---
+++ opps_proj.py
@@ -0,0 +1,26 @@
+class chatbook:
+    def __init__(self):
+        self.username=''
+        self.password=''
+        self.loggedin='False'
+        self.menu()
+
+    def menu(self):
+        user_input=input(""""welcome to chat book !! How would you like to proceeed?
+                         1. Press 1 for signup
+                         2. Press 2 to Sign in
+                         3. Press 3 to write a post
+                         4. Press 4 to message as friend
+                         5. Press any other key to exit """)
+        if user_input=='1':
+            pass
+        elif user_input=='2':
+            pass
+        elif user_input=='3':
+            pass
+        elif user_input=='4':
+            pass
+        else:
+            exit()
+
+obj=chatbook()
