7eb6bf6e69c0685956b854f0f928ce246b37ca6f
Basling <basling99@gmail.com>
2025-07-24 15:03:27 +0530

    class and obj basics done

--- oops1.py
+++ oops1.py
@@ -2,12 +2,20 @@
 class Employee:
     #Special fun/method, Magic methods,Dunder method/Constructor
     def __init__(self):
+        print("Started executing attributes/data")
         self.Salary=50000
         self.ID=23
         self.Designation="Staff software engineer"
+        print("attributes and data has been initiated")
+
+    def Travel(self,Destination):
+        print("This travel function/method is called manually")
+        print(f"Employee is now travelling to {Destination}")
 
 #Creating an Obj of a class
 sam=Employee()
-print(sam.ID)
-print(sam.Designation)
-print(sam.Salary)
+print(type(sam))
+#sam.Travel("Kerala")
+# print(sam.ID)
+# print(sam.Designation)
+# print(sam.Salary)
